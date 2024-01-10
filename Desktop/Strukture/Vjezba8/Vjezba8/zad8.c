56915a3110cbacbba8f51dcae3529ec8563100ea
MatejVuj <vujmatej@gmail.com>
2024-01-10 17:10:46 +0100

    zad8i9

--- Desktop/Strukture/Vjezba8/Vjezba8/zad8.c
+++ Desktop/Strukture/Vjezba8/Vjezba8/zad8.c
@@ -3,11 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX_NAME 50
 #define MAX_LINE 256
 #define EXIT_ERROR -1
 #define EXIT_SUCCESS 0
+#define LOWER 10
+#define UPPER 90
 
 struct node;
 typedef struct node* Position;
@@ -21,13 +24,22 @@ typedef struct node {
 
 int menu(int);
 int Inorder(Position);
+int InorderFile(Positionb);
 int Preorder(Position);
 int Postorder(Position);
 int Levelorder(Position);
 int Height(Position);
 int CurrentLevel(Position, int);
+int Replace(Position);
+int rnd();
+int PrintInFile(Position, char*);
 Position Insert(Position, Position);
 Position Create(Position, int);
+Position FindElement(Position, int);
+Position DeleteElement(Position, int);
+Position FindMax(Position);
+Position FindMin(Position);
+int DeleteAll(Position);
 
 int main(){
 
@@ -36,15 +48,45 @@ int main(){
 
 	int insert = 0, choise = 0;
 	int number = 0;
+	int i = 0, n = 10;
+
+	//srand(unsigned(time(0)));
+
+	Position element;
+	element = (Position)malloc(sizeof(Node));
 
 	while (choise != 9) {
 		choise = menu(insert);
 		switch (choise) {
-		case 1:
+		/*case 1:
 			printf("\n\tUnesite broj\t");
 			scanf("%d", &number);
 			q = Create(Root, number);
 			Root = Insert(Root,q);
+			break;*/
+
+		case 1:
+			printf("\n\tUnosenje brojeva\t");
+			q = Create(Root, 2);
+			Root = Insert(Root, q);
+			q = Create(Root, 5);
+			Root = Insert(Root, q);
+			q = Create(Root, 7);
+			Root = Insert(Root, q);
+			q = Create(Root, 8);
+			Root = Insert(Root, q);
+			q = Create(Root, 11);
+			Root = Insert(Root, q);
+			q = Create(Root, 1);
+			Root = Insert(Root, q);
+			q = Create(Root, 4);
+			Root = Insert(Root, q);
+			q = Create(Root, 2);
+			Root = Insert(Root, q);
+			q = Create(Root, 3);
+			Root = Insert(Root, q);
+			q = Create(Root, 7);
+			Root = Insert(Root, q);
 			break;
 		case 2:
 			printf("\n\tInorder: ");
@@ -67,8 +109,38 @@ int main(){
 			printf("\n");
 			break;
 
-		case 9:
+		case 6:
+			printf("\n\tKoji element zelite izbrisati");
+			scanf("%d", &number);
+			Root = DeleteElement(Root, number);
+			printf("\n");
 			break;
+
+		case 7:
+			printf("\n\tKoji element zelite pronaci");
+			scanf("%d", &number);
+			element = FindElement(Root, number);
+			if (element != NULL)
+				printf("Element %d is found!\n", number);
+			break;
+
+		case 8:
+			Replace(Root);
+			break;
+
+		case 9:
+			InorderFile(Root);
+			break;
+
+		case 11:
+			for (i = 0; i < 10; i++) {
+				q = Create(Root, rnd());
+				Root = Insert(Root, q);
+			}
+			break;
+
+		case 101:
+			DeleteAll(Root);
 
 		default:
 			printf("\n\tNiste odabrali mogucu NAREDBU");
@@ -88,7 +160,12 @@ int menu(int choise) {
 	printf("\n\t3 -> Ispis preorder");
 	printf("\n\t4 -> Ispis postorder");
 	printf("\n\t5 -> Ispis level order");
-	printf("\n\t9 -> Izlaz");
+	printf("\n\t6 -> Izbrisi element");
+	printf("\n\t7 -> pronadji element");
+	printf("\n\t8 -> replace");
+	printf("\n\t9 -> Ispis Inordera u file");
+	printf("\n\t11 -> Random borjevi");
+	printf("\n\t101 -> Izbrisi sve");
 	printf("\n\tNaredba:\t");
 	scanf("%d", &choise);
 
@@ -124,7 +201,6 @@ Position Insert(Position p, Position q) {
 
 	return p;
 }
-
 
 int Inorder(Position p) {
 
@@ -211,4 +287,143 @@ int CurrentLevel(Position p, int level) {
 
 	return EXIT_SUCCESS;
 
+}
+
+Position FindElement(Position p, int data) {
+
+	if (p == NULL) {
+		printf("Element ne postoji");
+		return NULL;
+	}
+
+	if (p->el == data)
+		return p;
+
+	else if (p->el > data) {
+		p->left = FindElement(p->el, data);
+		return p;
+	}
+
+	else {
+		p->right = FindElement(p->el, data);
+		return p;
+	}
+
+}
+
+Position DeleteElement(Position p, int data) {
+	
+	if (p == NULL)
+		return NULL;
+	
+	if (p->el < data)
+		p->right = DeleteElement(p->right, data);
+
+	else if (p->el > data)
+		p->left = DeleteElement(p->left, data);
+
+	else {
+		
+		if (p->left) {
+			Position temp = FindMax(p->left);
+			p->el = temp->el;
+			p->left = DeleteElement(p->left, temp->el);
+		}
+
+		else if (p->right) {
+			Position temp = FindMin(p->right);
+			p->el = temp->el;
+			p->right = DeleteElement(p->right, temp->el);
+		}
+
+		else {
+			free(p);
+			return NULL;
+		}
+	}
+	return p;
+}
+
+Position FindMax(Position p) {
+
+	if (p == NULL) {
+		printf("Error");
+		return NULL;
+	}
+
+	while (p->left != NULL)
+		p = p->left;
+
+	return p;
+}
+
+Position FindMin(Position p) {
+
+	if (p == NULL) {
+		printf("Error");
+		return NULL;
+	}
+
+	while (p->right != NULL)
+		p = p->right;
+
+	return p;
+}
+
+int DeleteAll(Position p) {
+	
+	if (p == NULL)
+		return EXIT_SUCCESS;
+
+	DeleteAll(p->right);
+	DeleteAll(p->left);
+	free(p);
+	return EXIT_SUCCESS;
+}
+
+int Replace(Position p) {
+
+	if (p == NULL)
+		return EXIT_SUCCESS;
+
+	int temp = p->el;
+	p->el = Replace(p->left) + Replace(p->right);
+
+	return p->el + temp;
+}
+
+int rnd() {
+	
+	int number = 0;
+	number = (rand() % (UPPER - LOWER)) + LOWER;
+	return number;
+}
+
+int InorderFile(Position p) {
+
+	char file[MAX_NAME] = {"inorder.txt"};
+
+	FILE* fp = NULL;
+	fp = fopen(file, "w");
+
+	if (fp == NULL)
+	{
+		printf("Error in file opening!\n");
+		return EXIT_ERROR;
+	}
+
+	else {
+
+		if (p == NULL)
+			return EXIT_SUCCESS;
+
+		Inorder(p->left, fp);
+		fprintf(fp, "%d ", p->el);
+		Inorder(p->right, fp);
+		return EXIT_SUCCESS;
+
+		fclose(fp);
+
+		return EXIT_SUCCESS;
+	}
 }
