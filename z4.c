e0f7b69695da038e412f8a919dd6ad28a13d7bc2
Matej Vujevic <147540118+MatejVuj@users.noreply.github.com>
2024-01-12 23:53:24 +0100

    Update z4.c

--- z4.c
+++ z4.c
@@ -1,346 +1,237 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+#define GRESKA_ALOKACIJE (-1)
+#define GRESKA_OTVARANJA (-2)
+#define MAX_FILE_NAME (256)
+#define MAX_RED (1024)
 
-#define MAX 256
-#define FILE_ERROR -1
-#define EXIT_SUCCESS 0
-
-struct polinom;
-typedef struct polinom* Position;
-typedef struct polinom {
-
-	int koef;
-	int exp;
-	Position Next;
-
+struct Polinom;
+typedef struct Polinom* Position;
+typedef struct Polinom {
+	int Koef;
+	int Pot;
+	Position next;
 }Polinom;
-
-int Pointers(Position, Position);
-int ReadFile(Position, char*);
-int InsertSort(Position, Position);
-int ZbrojiPolinome(Position, Position, Position);
-int PomnoziPolinome(Position, Position, Position);
-int IspisListe(Position);
-int BrisiSve(Position);
-
-Position Allocate();
-Position NewEnter(int, int);
+int CitajPolinome(char* ime_dat, Position p, Position q);
+int UnosSort(int koef, int pot, Position p);
+int MnoziPolinome(Position prvi, Position drugi, Position umnozak);
+int ZbrojiPolinome(Position prvi, Position drugi, Position zbroj);
+int ZbrojiPomnozeno(Position p);
+int Brisi(int a, Position p);
+int BrisiSve(Position p);
 
 int main() {
-
-	Position Head1 = NULL;
-	Position Head2 = NULL;
-	Position Head3 = NULL;
-	Position Head4 = NULL;
-
-
-	char file[MAX] = {"potencije.txt"};
-
-	Head1 = Allocate();
-	Head2 = Allocate();
-	Head3 = Allocate();
-	Head4 = Allocate();
-	
-	ReadFile(Head1, file);
-
-	ZbrojiPolinome(Head1->Next,Head2->Next,Head3->Next);
-
-	IspisListe(Head3->Next);
-
-	PomnoziPolinome(Head1->Next, Head2->Next, Head4->Next);
-
-	IspisListe(Head4->Next);
-
-	BrisiSve(Head1);
-	BrisiSve(Head2);
-	BrisiSve(Head3);
-	BrisiSve(Head4);
-
-	return 0;
-}
-
-Position Allocate() {
-
-	Position Head = NULL;
-
-	Head = (Position)malloc(sizeof(Polinom));
-
-	if (Head == NULL) {
-	
-		printf("Neuspjesno allocirana memorija");
-		return NULL;
-	}
-
-	return Head;
-}
-
-int Pointers(Position p, Position q) {
-
-	q->Next = p->Next;
-	p->Next = q;
-
+	Polinom head1 = {
+		.Koef = {0},
+		.Pot = {0},
+		.next = NULL
+	};
+	Polinom head2 = {
+		.Koef = {0},
+		.Pot = {0},
+		.next = NULL
+	};
+	Polinom head3 = {
+		.Koef = {0},
+		.Pot = {0},
+		.next = NULL
+	};
+	Polinom head4 = {
+		.Koef = {0},
+		.Pot = {0},
+		.next = NULL
+	};
+	char ime_dat[MAX_FILE_NAME] = { 0 };
+	printf("Molimo unesite ime datoteke iz koje zelite procitati polinome:\t");
+	scanf(" %s", ime_dat);
+	if (CitajPolinome(ime_dat, &head1, &head2) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	if (ZbrojiPolinome(&head1, &head2, &head3) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	if (MnoziPolinome(&head1, &head2, &head4) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	printf("Prvi polinom:\t");
+	Ispis(&head1);
+	printf("\nDrugi polinom:\t");
+	Ispis(&head2);
+	printf("\nZbroj polinoma:\t");
+	Ispis(&head3);
+	printf("\nUmnozak polinoma:\t");
+	Ispis(&head4);
+	BrisiSve(&head1);
+	BrisiSve(&head2);
+	BrisiSve(&head3);
+	BrisiSve(&head4);
 	return EXIT_SUCCESS;
 }
+int CitajPolinome(char* ime_dat, Position p, Position q) {
 
-int ReadFile(Position Head, char* file) {
-
-	int koef = 0;
-	int exp = 0;
-	int temp = 0;
-	int counter = 0;
-	char buffer[MAX] = { 0 };
-	char* p_buffer = buffer;
-
-	FILE* f;
-	f = fopen(file, "r");
-
-	if (f == NULL) {
-		
-		printf("Greska u otvaranju datoteke\n");
-		return FILE_ERROR;
+	FILE* fp = NULL;
+	fp = fopen(ime_dat, "r");
+	char buffer[MAX_RED] = { 0 };
+	int pot, koef = 0;
+	char* pok = buffer;
+	int n, tmp = 0;
+	if (fp == NULL) {
+		printf("Postovani, datoteka %s se nije ispravno otvorila!\r\n", ime_dat);
+		return GRESKA_OTVARANJA;
 	}
-	
-	fgets(p_buffer, MAX, f);
 
-	while (strlen(p_buffer) != 0) {
-		
-		temp = sscanf(p_buffer, "%dx^%d %n", &koef, &exp, &counter);
+	fgets(buffer, MAX_RED, fp);
+	tmp = strlen(buffer);
+	while (tmp > 0) {
+		sscanf(pok, "%d %d %n", &koef, &pot, &n);
+		if (UnosSort(koef, pot, p) != EXIT_SUCCESS)
+			return EXIT_FAILURE;
+		pok += n;
+		tmp = tmp - n;
 
-		if (temp == 2) {
-			
-			Position El = NULL; //clan liste
-			El = (Position)malloc(sizeof(Polinom));
-			
-			El->koef = koef;
-			El->exp = exp;
-			El->Next = NULL;
-
-			InsertSort(Head, El);
-
-			printf("%dx^%d ", El->koef, El->exp);
+	}
+	fgets(buffer, MAX_RED, fp);
+	tmp = strlen(buffer);
+	pok = buffer;
+	while (tmp > 0) {
+		sscanf(pok, "%d %d %n", &koef, &pot, &n);
+		if (UnosSort(koef, pot, q) != EXIT_SUCCESS)
+			return EXIT_FAILURE;
+		pok += n;
+		tmp = tmp - n;
+	}
+	fclose(fp);
+	return EXIT_SUCCESS;
+}
+int UnosSort(int koef, int pot, Position p) {
+	while (p->next != NULL && p->next->Pot > pot)
+		p = p->next;
+	Position q = (Position)malloc(sizeof(struct Polinom));
+	if (q == NULL)
+		return GRESKA_ALOKACIJE;
+	q->Koef = koef;
+	q->Pot = pot;
+	q->next = p->next;
+	p->next = q;
+	return EXIT_SUCCESS;
+}
+int Ispis(Position p) {
+	while (p->next != NULL) {
+		printf("%d  %d\t", p->next->Koef, p->next->Pot);
+		p = p->next;
+	}
+	return EXIT_SUCCESS;
+}
+int MnoziPolinome(Position prvi, Position drugi, Position umnozak) {
+	Position rezultat = umnozak;
+	prvi = prvi->next;
+	drugi = drugi->next;
+	Position pocetak = drugi;
+	int koef, pot = 0;
+	while (prvi != NULL) {
+		drugi = pocetak;
+		while (drugi != NULL) {
+			koef = prvi->Koef * drugi->Koef;
+			pot = prvi->Pot + drugi->Pot;
+			if (UnosSort(koef, pot, umnozak) != EXIT_SUCCESS)
+				return EXIT_FAILURE;
+			drugi = drugi->next;
 		}
+		prvi = prvi->next;
+	}
+	if (ZbrojiPomnozeno(rezultat) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}
+int ZbrojiPolinome(Position prvi, Position drugi, Position zbroj) {
+	prvi = prvi->next;
+	drugi = drugi->next;
+	int koef, pot = 0;
+	while (prvi != NULL && drugi != NULL) {
 
+		if (prvi->Pot == drugi->Pot) {
+			koef = prvi->Koef + drugi->Koef;
+			pot = prvi->Pot;
+			prvi = prvi->next;
+			drugi = drugi->next;
+
+		}
+		else if (prvi->Pot > drugi->Pot) {
+			koef = prvi->Koef;
+			pot = prvi->Pot;
+			prvi = prvi->next;
+		}
 		else {
-		
-			printf("Greska u citanju\n");
+			koef = drugi->Koef;
+			pot = drugi->Pot;
+			drugi = drugi->next;
 		}
-
-		p_buffer += counter;
+		if (UnosSort(koef, pot, zbroj) != EXIT_SUCCESS)
+			return EXIT_FAILURE;
 	}
+	while (prvi != NULL) {
+		koef = prvi->Koef;
+		pot = prvi->Koef;
+		if (UnosSort(koef, pot, zbroj) != EXIT_SUCCESS)
+			return EXIT_FAILURE;
+		prvi = prvi->next;
+	}
+	while (drugi != NULL) {
+		koef = drugi->Koef;
+		pot = drugi->Koef;
+		if (UnosSort(koef, pot, zbroj) != EXIT_SUCCESS)
+			return EXIT_FAILURE;
+		drugi = drugi->next;
+	}
+	return EXIT_SUCCESS;
 
+}
+int ZbrojiPomnozeno(Position p) {
+	Position pocetak = p;
+	p = p->next;
+	if (p != NULL) {
+		while (p->next != NULL) {
+			if (p->Pot == p->next->Pot) {
+				p->Koef = p->Koef + p->next->Koef;
+				if (Brisi(p->Pot, pocetak) != EXIT_SUCCESS)
+					return EXIT_FAILURE;
+			}
+			if (p->Pot != p->next->Pot)//Ukoliko postoje tri ili više ista pribrojnika
+				p = p->next;
+
+		}
+	}
+	else {
+		printf("Nema elemenata");
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
-
-int InsertSort(Position p, Position q) {
-
-	printf("Inserting node with koef = %d and exp = %d\n", q->koef, q->exp);
-
-
+int Brisi(int a, Position p) {
 	Position temp = NULL;
-	
-	temp = (Position)malloc(sizeof(Polinom));
-
-	if (temp == NULL) {
-		printf("Greska u allokaciji\n");
-		return FILE_ERROR;
+	p = p->next;
+	if (p == NULL) {
+		printf("Prazna lista");
+		return EXIT_FAILURE;
 	}
-
-	temp->Next = NULL;
-
-	if (p->koef == 0 || (p->koef + q->koef == 0)) {
-		
-		printf("Koeficijent je 0\n");
+	while (p->next != NULL && p->Pot != a)
+		p = p->next;
+	if (p == NULL) {
+		printf("Nema");
+		return EXIT_FAILURE;
 	}
-
-	while (p->Next != NULL && p->Next->exp > q->exp) {
-		
-		p = p->Next;
-	}
-
-	if (p->Next != NULL && p->Next->exp == q->exp) {
-	
-		p->Next->koef += q->koef;
-
+	temp = p->next;
+	p->next = temp->next;
+	free(temp);
+	return EXIT_SUCCESS;
+}
+int BrisiSve(Position p) {
+	Position q;
+	while (p->next != NULL) {
+		q = p->next;
+		p->next = q->next;
 		free(q);
 	}
 
-	else {
-	
-		temp = NewEnter(temp->koef, temp->exp);
-		Pointers(p, temp);
-
-	}
-
-	return EXIT_SUCCESS;
-}
-
-Position NewEnter(int koef, int exp) {
-
-	printf("Creating new node with koef = %d and exp = %d\n", koef, exp);
-
-
-	Position Head = (Position)malloc(sizeof(Polinom));
-
-	if (Head == NULL) {
-
-		printf("Greska u alokaciji memorije");
-		return NULL;
-	}
-
-	Head->koef = koef;
-	Head->exp = exp;
-	Head->Next = NULL;
-
-	return Head;
-
-}
-
-int ZbrojiPolinome(Position Head1, Position Head2, Position Head3) {
-
-	Position temp = NULL;
-	Position New = NULL;
-
-	temp = (Position)malloc(sizeof(Polinom));
-
-	if (Head1 == NULL || Head2 == NULL) {
-	
-		printf("Polinom je 0");
-		return FILE_ERROR;
-
-	}
-
-	while (Head1 != NULL && Head2 != NULL) {
-		
-		if (Head1->exp > Head2->exp) {
-			
-			New = NewEnter(Head1->koef, Head1->exp);
-
-			InsertSort(Head3, New);
-
-			Head1 = Head1->Next;
-		}
-
-		else if (Head1->exp < Head2->exp) {
-		
-			//New = NULL;
-			
-			New = NewEnter(Head2->koef, Head2->exp);
-			
-			InsertSort(Head3, New);
-			
-			Head2 = Head2->Next;
-		
-		}
-
-		else if (Head1->exp == Head2->exp) {
-		
-			//New = NULL;
-
-			New = NewEnter(Head1->koef + Head2->koef, Head1->exp);
-
-			InsertSort(Head3, New);
-
-			Head1 = Head1->Next;
-
-			Head2 = Head2->Next;
-		}
-	}
-
-	/*while (Head1 != NULL) {
-		Position node = NewEnter(Head1->koef, Head1->exp);
-		InsertSort(Head3, node);
-		Head1 = Head1->Next;
-	}
-
-	while (Head2 != NULL) {
-		Position node = NewEnter(Head2->koef, Head2->exp);
-		InsertSort(Head3, node);
-		Head2 = Head2->Next;
-	}*/
-
-
-	temp = Allocate();
-
-	if (Head2 == NULL) {
-		temp = Head1;
-	}
-
-	else if (Head1 == NULL) {
-		temp = Head2;
-	}
-
-	while (temp != NULL) {
-		
-		New = NewEnter(temp->koef, temp->exp);
-
-		InsertSort(Head3, New);
-
-		temp = temp->Next;
-	}
-
-	return EXIT_SUCCESS;
-}
-
-int PomnoziPolinome(Position P1, Position P2, Position P)
-{
-	Position poz1 = P1;
-	Position poz2 = P2;
-	Position p3 = NULL;
-
-	p3 = (Position)malloc(sizeof(Polinom));
-
-	while (poz1 != NULL)
-	{
-		poz2 = P2;
-		while (poz2 != NULL)
-		{
-			p3 = NewEnter(poz1->koef * poz2->koef, poz1->exp + poz2->exp);
-			InsertSort(P, poz1);
-			poz2 = poz2->Next;
-		}
-		poz1 = poz1->Next;
-	}
-
-	return EXIT_SUCCESS;
-}
-
-int IspisListe(Position HeadN) {
-
-	printf("Printing list...\n");
-
-	if (HeadN == NULL) {
-
-		printf("Lista je prazna\n");
-		return FILE_ERROR;
-	}
-
-	while (HeadN != NULL) {
-		printf("%d x^%d + ", HeadN->koef, HeadN->exp);
-		HeadN = HeadN->Next;
-	}
-
-	printf("\n");
-
-	return EXIT_SUCCESS;
-
-}
-
-int BrisiSve(Position Head) {
-
-	Position temp = NULL;
-
-	while (Head->Next != NULL) {
-		
-		temp = Head->Next;
-		Head->Next = temp->Next;
-		free(temp);
-	}
-
-	Head->Next = NULL;
-
-	return EXIT_SUCCESS;
 }
